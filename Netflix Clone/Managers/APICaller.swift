3c4c379f0aa954e05db727675374a556ffb6c91f
Kuntulu Ankita <kuntuluankita@gmail.com>
2023-07-25 22:14:04 +0530

    Created  New API _Key

---
+++ Netflix Clone/Managers/APICaller.swift
@@ -0,0 +1,20 @@
+//
+//  APICaller.swift
+//  Netflix Clone
+//
+//  Created by K Praveen Kumar on 25/07/23.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_Key = "3c3895e1cba7ab9c182d5d7df5b89610"
+}
+
+class APICaller {
+    static let shared = APICaller()
+}
+
+
+
+
